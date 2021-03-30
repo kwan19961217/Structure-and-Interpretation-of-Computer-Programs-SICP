8954c54def7bb46e8da7710e8bf7d0e9148426d2
kwan19961217 <59208523+kwan19961217@users.noreply.github.com>
2021-03-30 22:39:38 +0800

    Update ex1_17.scm

--- CH1/ex1_17.scm
+++ CH1/ex1_17.scm
@@ -1,17 +1,17 @@
 (define (fast-iter b n a)
-		  (cond ((= n 1) (+ b a))
-		  ((even? n) (fast-iter (double b)
-					                  (halve n)
-					                  a))
-		  (else (fast-iter b 
-                       (- n 1) 
-                       (+ a b)))))
+	(cond ((= n 1) (+ b a))
+	      ((even? n) (fast-iter (double b)
+				    (halve n)
+				    a))
+	      (else (fast-iter b 
+                      	       (- n 1) 
+                      	       (+ a b)))))
 
 (define (halve a)
-		    (/ a 2))
+	(/ a 2))
 
 (define (double a)
-		    (+ a a))
+	(+ a a))
 
 (define (even? n)
-		    (= (remainder n 2) 0))
+	(= (remainder n 2) 0))
