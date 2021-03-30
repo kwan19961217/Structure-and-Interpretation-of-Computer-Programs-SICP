65eb393c17094cf45b2fe38f053482f1261a6ea6
kwan19961217 <59208523+kwan19961217@users.noreply.github.com>
2021-03-30 18:16:06 +0800

    Create ex1_16.scm

---
+++ CH1/ex1_16.scm
@@ -0,0 +1,20 @@
+(define (square a)
+		    (* a a))
+
+(define (fast-expt b n a)
+		    (cond ((= n 0) a)
+			  ((even? n) (fast-expt b 
+                              (- n 2) 
+                              (* a 
+                                 (square b))))
+              
+			  (else (fast-expt b 
+                         (- n 1) 
+                         (* a b)))))
+
+(define (even? n)
+		    (= (remainder n 2) 0))
+
+(define (fast-expt-iter b n)
+        (fast-expt b n 1))
+
