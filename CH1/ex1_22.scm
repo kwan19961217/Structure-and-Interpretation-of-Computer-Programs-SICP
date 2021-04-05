669169fe9c18ba40c199c5572a3840d296a700b7
kwan19961217 <59208523+kwan19961217@users.noreply.github.com>
2021-04-05 16:45:07 +0800

    Create ex1_22.scm

---
+++ CH1/ex1_22.scm
@@ -0,0 +1,39 @@
+(define (search-for-primes n) 
+	(if (= (remainder n 2) 0)
+		(prime-iter (+ n 1) 0)
+		(prime-iter n 0)))
+
+	(define (prime-iter n count)
+		(cond ((= count 3) True)
+		 	  ((= (smallest-divisor n) n) (prime-iter (+ n 2) (+ count 1)))
+			  (else (prime-iter (+ n 2) count))))
+
+
+  
+  
+  
+  
+  
+  
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+(define (start-prime-test n start-time)
+  (if (search-for-primes n)
+      (report-prime (- (runtime) start-time))))
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+
+
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b)
+  (= (remainder b a) 0))
