7704018e1143082d6ce240b235c10eb11827027a
kwan19961217 <59208523+kwan19961217@users.noreply.github.com>
2021-04-13 11:15:22 +0800

    Update ex1_33.scm

--- CH1/ex1_33.scm
+++ CH1/ex1_33.scm
@@ -1,3 +1,4 @@
+;General Function
 (define (filtered-accumulate filter combiner null-value term a next b)
 	(define (recur term a next b)
 		(if (> a b)
@@ -8,7 +9,21 @@
       			(recur term (next a) next b))))
 	(recur term a next b))
 
+
+
 ;a using answer in ex1_28
-(filtered-accumulate prime-test + 0 square 2 add-one 10)
+(define (sum-of-square-of-primes a b) (filtered-accumulate prime-test + 0 square a add-one b))
 
 ;b
+(define (product-of-relative-primes n)
+	    (define (filter x)
+	    	    (= (gcd n x) 1))
+	    (filtered-accumulate filter * 1 iden 1 add-one n))
+
+;misc.
+(define (add-one x) (+ x 1))
+(define (iden x) x)
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
