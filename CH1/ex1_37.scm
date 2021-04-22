4d4643aa8368337f282d2b6bfaed62ee816356a8
kwan19961217 <59208523+kwan19961217@users.noreply.github.com>
2021-04-22 23:06:11 +0800

    Update ex1_37.scm

--- CH1/ex1_37.scm
+++ CH1/ex1_37.scm
@@ -8,13 +8,11 @@
 
 ;iterative
 (define (cont-frac n d k)
-
-(cont-frac (lambda (i) 1.0)
-           (lambda (i) 1.0)
-           k)
-
-(define (cont-frac n d k)
 	(define (iter x result)
 		(if (= x 0) result
 			(iter (- x 1) (/ (n x) (+ (d x) result)))))
 	(iter k 0))
+
+(cont-frac (lambda (i) 1.0)
+           (lambda (i) 1.0)
+           k)
