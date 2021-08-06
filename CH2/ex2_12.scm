da47db67e5adcee9e0f97164262197d7dba63cef
kwan19961217 <59208523+kwan19961217@users.noreply.github.com>
2021-08-07 00:17:08 +0800

    Update ex2_12.scm

--- CH2/ex2_12.scm
+++ CH2/ex2_12.scm
@@ -1,15 +1,20 @@
 (define (make-center-percent c p)
-        (make-interval (- c (* c p))
-                       (+ c (* c p))))
+        (make-center-width c (* c p)))
 
 (define (percent i)
-        (/ (- (upper-bound i)
-              (center i))
+        (/ (width i)
            (center i)))
 
 ;Below are from lib
  (define (make-interval a b) (cons a b)) 
  (define (upper-bound interval) (max (car interval) (cdr interval))) 
  (define (lower-bound interval) (min (car interval) (cdr interval))) 
+  
  (define (center i)
          (/ (+ (lower-bound i) (upper-bound i)) 2))
+  
+(define (make-center-width c w)
+        (make-interval (- c w) (+ c w)))
+  
+(define (width i)
+        (/ (- (upper-bound i) (lower-bound i)) 2))
