4874235b27375035957eb73b170fcb9e4993cd6c
kwan19961217 <59208523+kwan19961217@users.noreply.github.com>
2021-08-06 23:25:57 +0800

    Update ex2_9.scm

--- CH2/ex2_9.scm
+++ CH2/ex2_9.scm
@@ -1,10 +1,10 @@
 (define (div-interval x y)
   (if (and (< (upper-bound y) 0)
-  		   (< (lower-bound y) 0))
+  		     (< (lower-bound y) 0))
   	  (display  "error")
   	  (mul-interval x 
-                	(make-interval (/ 1.0 (upper-bound y))
-                     	           (/ 1.0 (lower-bound y))))))
+                	  (make-interval (/ 1.0 (upper-bound y))
+                     	             (/ 1.0 (lower-bound y))))))
 
 ;Below are from lib
  (define (make-interval a b) (cons a b)) 
