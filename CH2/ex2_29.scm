e512ff706c741b0782faf2af16b51bcb7ad09ff8
kwan19961217 <59208523+kwan19961217@users.noreply.github.com>
2021-08-15 23:02:40 +0800

    Update ex2_29.scm

--- CH2/ex2_29.scm
+++ CH2/ex2_29.scm
@@ -16,3 +16,32 @@
                 	(total-weight (branch-structure br))
                 ))
         (+ (weight (left-branch mobile)) (weight (right-branch mobile))))
+
+;c
+(define (torque br)
+        (* (weight br) (branch-length br)))
+(define (weight br)
+        (if (not (pair? (branch-structure br)))
+            (branch-structure br)
+            (total-weight (branch-structure br))))
+
+(define (equal? mobile)
+        (= (torque (left-branch mobile))
+           (torque (right-branch mobile))))
+
+(define (balance? mobile)
+        (if (not (pair? mobile))
+            #t
+            (and (equal? mobile)
+                 (balance? (branch-structure (left-branch mobile)))
+                 (balance? (branch-structure (right-branch mobile))))))
+
+;d
+(define (left-branch mobile)
+        (car mobile))
+(define (right-branch mobile)
+        (cdr mobile))
+(define (branch-length br)
+        (car br))
+(define (branch-structure br)
+        (cdr br))
