c7786b8ee0aca702927eafdf789a2c491e1ec76a
kwan19961217 <59208523+kwan19961217@users.noreply.github.com>
2021-08-23 22:36:46 +0800

    Create ex2_47.scm

---
+++ CH2/ex2_47.scm
@@ -0,0 +1,23 @@
+;(define (make-frame origin edge1 edge2)
+;  (list origin edge1 edge2))
+
+(define (origin-frame frame)
+        (car frame))   
+
+(define (edge1-frame frame)
+        (car (cdr frame)))      
+
+(define (edge2-frame frame)
+        (car (cdr (cdr frame))))
+
+;(define (make-frame origin edge1 edge2)
+;  (cons origin (cons edge1 edge2)))
+
+(define (origin-frame frame)
+        (car frame))
+
+(define (edge1-frame frame)
+        (car (cdr frame)))
+
+(define (edge2-frame frame)
+        (cdr (cdr frame)))
