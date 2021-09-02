34b9f6c9482570dc66224d042132eadf566e13c7
kwan19961217 <59208523+kwan19961217@users.noreply.github.com>
2021-09-02 21:39:00 +0800

    Delete ex2.30.scm

--- CH2/ex2.30.scm
+++
@@ -1,5 +0,0 @@
-(define (square-tree tree)
-        (cond ((null? tree) nil)
-              ((not (pair? tree)) (square tree))
-              (else (cons (square-tree (car tree))
-                          (square-tree (cdr tree))))))
