3a58ec0175fccf323fa5961276905850f1aea7a5
kwan19961217 <59208523+kwan19961217@users.noreply.github.com>
2021-09-29 22:46:16 +0800

    Create ex2_75.scm

---
+++ CH2/ex2_75.scm
@@ -0,0 +1,11 @@
+(define (make-from-mag-ang m a)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) 
+           (* m (cos a)))
+          ((eq? op 'imag-part) 
+           (* m (sin a)))
+          ((eq? op 'magnitude) m)
+          ((eq? op 'angle) a)
+          (else
+           (error "Unknown op -- MAKE-FROM-MAG-ANG" op))))
+  dispatch)  
