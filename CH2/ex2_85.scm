0975dfcf1dead10b4bed14d0ec8020c7c80482b4
kwan19961217 <59208523+kwan19961217@users.noreply.github.com>
2021-10-02 21:55:56 +0800

    Create ex2_85.scm

---
+++ CH2/ex2_85.scm
@@ -0,0 +1,29 @@
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+        (let ((proc (get op type-tags)))
+              (if proc
+                 ;add drop here to simplify output
+                 (drop (apply proc (map contents args)))
+                 (apply-coer-list args))))
+  
+  
+;project package
+(define install-project-package
+  (define (project-complex->real val)
+          (tag 'real (real-part val)))
+
+  (define (project-rational->integer val)
+          (numer val))
+
+  (define (project-real->integer val)
+          (round (contents val)))
+  
+  (put 'project 'complex (lambda (x) (project-complex->real x)))
+  (put 'project 'real (lambda (x) (project-real->integer x)))
+  (put 'project 'rational (lambda (x) (project-rational->integer x))))
+
+(define (drop val)
+        (let ((proc (get 'project (type-tag val)))
+              (if proc
+                  (drop (apply proc val))
+                  val))))
